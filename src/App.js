e8bfb403382aafd050c062a1aee5cae0a52e36a4
mwmMecoy <macmecoy@gmail.com>
2022-10-10 14:38:52 -0500

    Modified state change for adding new tasks and created build

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header  onAdd={() => setShowAddTask(!showAddTask)} />
+      <Header  onAdd={() => setShowAddTask(!showAddTask)} showAdd ={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks To Show"}
     </div> 
